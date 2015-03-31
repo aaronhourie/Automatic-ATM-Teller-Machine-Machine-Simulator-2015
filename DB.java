9ca74dd31fd0612534318c96c7bbc01ccae00e8c
frog- <nikmcdonald@outlook.com>
2015-03-31 12:37:53 -0300

    friggin around

---
+++ DB.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+public class DB {
+
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://192.168.2.25:3306/Cs2";
+
+	static final String USER = "java";
+	static final String PASS = "csci1101";
+
+	public static Connection connect() {
+		Connection conn = null;
+
+		try {
+			Class.forName(JDBC_DRIVER);
+			System.out.println("Connecting to db...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}	
+}
